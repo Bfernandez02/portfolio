0323c1bc2d8885687fd907d2210b477c93601439
Brandon Fernandez <athemstorm.08@gmail.com>
2026-01-19 17:52:43 -0500

    feat: enhance AboutMe component layout and styling for better responsiveness

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,48 +7,43 @@ import { useTheme } from "./ThemeProvider";
 export default function AboutMe() {
     const { theme } = useTheme();
   return (
-    <div className="w-fit rounded-[10px] flex flex-col md:flex-row gap-24 border-border border-2 px-15 py-10">
+    <div className="w-fit rounded-[10px] flex flex-col md:flex-row justify-between border-border border-2 lg:px-15 lg:py-10 px-5 py-5 max-md:items-center gap-10 md:gap-20">
       <div className="w-fit">
         <Image
           src={`/Brandon_${theme}.png`}
           alt="About Me Image"
           width={600}
           height={600}
-          className="w-full h-auto rounded-[10px] max-w-[500px]"
+          className="w-full h-auto rounded-[10px] md:max-w-[500px] max-w-[250px]"
         />
       </div>
 
       <div className="flex flex-col gap-8 w-full">
-        <div className="flex gap-16">
+        <div className="flex gap-4 xl:gap-12 sm:flex-row flex-col w-full justify-center">
           <div className="w-full border-border rounded-[10px] flex flex-col items-center  p-4 border-2">
             <FontAwesomeIcon
               icon={faAward}
-              className="text-4xl text-primary mb-4"
+              className="text-2xl md:text-4xl text-primary mb-4"
             />
-            <h6 className="text-center">3+ Years of fullstack experience</h6>
-            <h6 className="text-center">1+ Year of AI research</h6>
+            <h6 className="text-center max-lg:text-[16px]!">3+ Years of fullstack experience</h6>
+            <h6 className="text-center max-lg:text-[16px]!">1+ Year of AI research</h6>
           </div>
 
           <div className="w-full border-border rounded-[10px] flex flex-col items-center  p-4 border-2">
             <FontAwesomeIcon
               icon={faGraduationCap}
-              className="text-4xl text-primary mb-4"
+              className="text-2xl md:text-4xl text-primary mb-4"
             />
-            <h6 className="text-center">BSc (Honours) in Computer Science Co-op</h6>
-            <h6 className="text-center">MSc in Computer Science (in progress)</h6>
+            <h6 className="text-center max-lg:text-[16px]!">BSc (Honours) in Computer Science Co-op</h6>
+            <h6 className="text-center max-lg:text-[16px]!">MSc in Computer Science (in progress)</h6>
           </div>
         </div>
 
-        <p>
+        <p className="max-xl:text-[14px]!">
           Hello! I'm Brandon, a passionate software developer with a knack for
           creating dynamic and user-friendly web applications. With a strong
           foundation in both front-end and back-end technologies, I thrive on
-          transforming ideas into functional digital experiences. My journey in
-          coding began with a curiosity for how websites work, which quickly
-          evolved into a full-fledged career dedicated to continuous learning
-          and innovation. When I'm not coding, you can find me exploring the
-          latest tech trends, contributing to open-source projects, or indulging
-          in my love for photography. Let's build something amazing together!
+          transforming ideas into functional digital experiences.
         </p>
       </div>
     </div>
